956d8cf0f7993ae5e4059b6e509fb5a9a91c3a9e
SteveLorde <mostafa.maher98@gmail.com>
2024-06-01 12:21:03 +0200

    website seemingly done

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+const navbar = document.getElementById('navbar')
 const contentOutlet = document.getElementById('content')
 
 fetch("../../pages/home.html").then(res => res.text()).then(PageHTML => contentOutlet.innerHTML = PageHTML)
@@ -9,3 +10,18 @@ function SelectPage(number) {
         fetch("../../pages/about.html").then(res =>res.text()).then(PageHTML => contentOutlet.innerHTML = PageHTML)
     }
 }
+
+/*
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-200px"; // Adjust the value based on navbar height
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+ */
+
